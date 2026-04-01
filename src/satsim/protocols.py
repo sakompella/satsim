229a06e6e68f878e1dda1be3237f4904e3699473
sakompella <87831937+sakompella@users.noreply.github.com>
2026-04-01 18:34:51 -0400

    use np in protocol interfaces

--- src/satsim/protocols.py
+++ src/satsim/protocols.py
@@ -1,5 +1,7 @@
 from typing import Protocol, runtime_checkable
 
+import numpy as np
+
 from satsim.environment.earth import Environment
 from satsim.spacecraft import Spacecraft
 from satsim.types import StateVector, Vec3
@@ -7,16 +9,16 @@ from satsim.types import StateVector, Vec3
 
 @runtime_checkable
 class ForceModel(Protocol):
-    def acceleration(self, state: StateVector, t: float, env: Environment) -> Vec3: ...
+    def acceleration(self, state: StateVector, t: np.float64, env: Environment) -> Vec3: ...
 
 
 @runtime_checkable
 class StopCondition(Protocol):
-    def check(self, state: StateVector, t: float) -> bool: ...
+    def check(self, state: StateVector, t: np.float64) -> bool: ...
 
 
 @runtime_checkable
 class Event(Protocol):
-    trigger_time: float
+    trigger_time: np.float64
 
     def trigger(self, spacecraft: Spacecraft, env: Environment) -> None: ...
